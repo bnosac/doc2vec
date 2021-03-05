4462b6c4fcf12d2ca4e26dc6a306c21b83c1387d
Jan Wijffels <jwijffels@bnosac.be>
2021-03-05 17:41:38 +0100

    Print functionality for top2vec

--- R/pkg.R
+++ R/pkg.R
@@ -1,5 +1,6 @@
 #' @importFrom Rcpp evalCpp
 #' @importFrom stats predict median update
+#' @importFrom utils head
 #' @useDynLib doc2vec
 NULL
 
--- R/top2vec.R
+++ R/top2vec.R
@@ -5,6 +5,8 @@
 #' @param x either an object returned by \code{\link[doc2vec]{paragraph2vec}} or a data.frame 
 #' with columns `doc_id` and `text` storing document ids and texts as character vectors or a matrix with document embeddings to cluster
 #' or a list with elements docs and words containing document embeddings to cluster and word embeddings for deriving topic summaries
+#' @param data optionally, a data.frame with columns `doc_id` and `text` representing documents. 
+#' This dataset is just stored, in order to extract the text of the most similar documents to a topic.
 #' @param control.umap a list of arguments to pass on to \code{\link[uwot]{umap}} for reducing the dimensionality of the embedding space
 #' @param control.dbscan a list of arguments to pass on to \code{\link[dbscan]{hdbscan}} for clustering the reduced embedding space
 #' @param control.doc2vec optionally, a list of arguments to pass on to \code{\link[doc2vec]{paragraph2vec}} in case \code{x} is a data.frame
@@ -21,6 +23,7 @@
 #' \item{doc2vec: a doc2vec model}
 #' \item{umap: a matrix of representations of the documents of \code{x}}
 #' \item{dbscan: the result of the hdbscan clustering}
+#' \item{data: a data.frame with columns doc_id and text}
 #' }
 #' @examples 
 #' \dontshow{if(require(word2vec) && require(uwot) && require(dbscan))\{}
@@ -41,31 +44,37 @@
 #'                         threads = 1)
 #' # write.paragraph2vec(d2v, "d2v.bin")
 #' # d2v    <- read.paragraph2vec("d2v.bin")
-#' model  <- top2vec(d2v, 
+#' model  <- top2vec(d2v, data = x,
 #'                   control.dbscan = list(minPts = 50), 
 #'                   control.umap = list(n_neighbors = 15L, n_components = 4), trace = TRUE)
-#' model  <- top2vec(d2v, 
+#' model  <- top2vec(d2v, data = x,
 #'                   control.dbscan = list(minPts = 50), 
 #'                   control.umap = list(n_neighbors = 15L, n_components = 3), umap = tumap, 
 #'                   trace = TRUE)
 #'                                   
 #' info   <- summary(model, top_n = 7)
 #' info$topwords
+#' info$topdocs
 #' 
 #' ## Change the model: reduce doc2vec model to 2D
 #' model  <- update(model, type = "umap", 
 #'                  n_neighbors = 100, n_components = 2, metric = "cosine", umap = tumap, 
 #'                  trace = TRUE)
 #' info   <- summary(model, top_n = 7)
+#' data = x
 #' info$topwords
+#' info$topdocs
 #' 
 #' ## Change the model: have minimum 200 points for the core elements in the hdbscan density
 #' model  <- update(model, type = "hdbscan", minPts = 200, trace = TRUE)
 #' info   <- summary(model, top_n = 7)
+#' data = x
 #' info$topwords
+#' info$topdocs
 #' }
 #' \dontshow{\} # End of main if statement running only if the required packages are installed}
 top2vec <- function(x, 
+                    data = data.frame(doc_id = character(), text = character(), stringsAsFactors = FALSE), 
                     control.umap = list(n_neighbors = 15L, n_components = 5L, metric = "cosine"), 
                     control.dbscan = list(minPts = 100L), 
                     control.doc2vec = list(), 
@@ -74,6 +83,7 @@ top2vec <- function(x,
   requireNamespace("uwot")
   requireNamespace("dbscan")
   stopifnot(inherits(x, c("data.frame", "paragraph2vec", "paragraph2vec_trained", "matrix", "list")))
+  model <- NULL
   if(inherits(x, "data.frame")){
     stopifnot(all(c("doc_id", "text") %in% colnames(x)) && is.character(x$text))
     control.doc2vec$x <- x
@@ -119,8 +129,9 @@ top2vec <- function(x,
                         umap = embedding_umap,
                         umap_FUN = umap,
                         dbscan = clusters,
+                        data = data,
                         size = table(clusters$cluster),
-                        k = length(unique(model$dbscan$cluster)),
+                        k = length(unique(clusters$cluster)),
                         control = list(doc2vec = control.doc2vec, umap = control.umap, dbscan = control.dbscan)), 
                    class = "top2vec")
   out
@@ -164,6 +175,7 @@ update.top2vec <- function(object, type = c("umap", "hdbscan"),
 #' @param object an object of class \code{top2vec} as returned by \code{\link{top2vec}}
 #' @param type a character string with the type of summary information to extract. Currently not used
 #' @param top_n integer indicating to find the \code{top_n} most similar words to a topic
+#' @param data a data.frame with columns `doc_id` and `text` representing documents. For each topic, the function extracts the most similar documents.
 #' @param embedding_words a matrix of word embeddings to limit the most similar words to. Defaults to 
 #' the embedding of words from the \code{object}
 #' @param embedding_docs a matrix of document embeddings to limit the most similar documents to. Defaults to 
@@ -172,8 +184,13 @@ update.top2vec <- function(object, type = c("umap", "hdbscan"),
 #' @export
 #' @examples 
 #' # For an example, look at the documentation of ?top2vec
-summary.top2vec <- function(object, type = "default", top_n = 10, embedding_words = object$embedding$words, 
+summary.top2vec <- function(object, 
+                            type = "default", top_n = 10, data = object$data, embedding_words = object$embedding$words, 
                             embedding_docs = object$embedding$docs, ...){
+  recode <- function(x, from, to){
+    to[match(x, from)]
+  }
+  
   type <- match.arg(type)
   topic_idx       <- split(x = seq_along(object$dbscan$cluster), f = object$dbscan$cluster)
   topic_centroids <- lapply(topic_idx, FUN = function(i) colMeans(object$embedding$docs[i, , drop = FALSE]))
@@ -181,12 +198,38 @@ summary.top2vec <- function(object, type = "default", top_n = 10, embedding_word
   #topic_medoids <- do.call(rbind, topic_medoids)
   topwords <- lapply(topic_centroids, FUN = function(topic){
     similarity <- doc2vec::paragraph2vec_similarity(y = embedding_words, x = topic, top_n = top_n)
-    data.frame(term = similarity$term2, similarity = similarity$similarity, rank = similarity$rank, stringsAsFactors = FALSE)
+    data.frame(term       = similarity$term2, 
+               similarity = similarity$similarity, 
+               rank       = similarity$rank, stringsAsFactors = FALSE)
   })
   topdocs <- lapply(topic_centroids, FUN = function(topic){
     similarity <- doc2vec::paragraph2vec_similarity(y = embedding_docs, x = topic, top_n = top_n)
-    data.frame(doc_id = similarity$term2, similarity = similarity$similarity, rank = similarity$rank, stringsAsFactors = FALSE)
+    data.frame(doc_id     = similarity$term2, 
+               text       = recode(similarity$term2, from = data$doc_id, to = data$text), 
+               similarity = similarity$similarity, 
+               rank       = similarity$rank, stringsAsFactors = FALSE)
   })
-  out <- structure(list(topwords = topwords, topdocs = topdocs, centroids = topic_centroids, medoids = topic_medoids), class = "t2v")
+  out <- structure(list(k = object$k, size = object$size, topwords = topwords, topdocs = topdocs, centroids = topic_centroids, medoids = topic_medoids), class = "top2vec_summary")
   out
+}
+
+
+#' @export
+print.top2vec_summary <- function(x, top_n = 3, ...){
+  if(length(top_n) == 1){
+    top_n <- c(top_n, top_n)
+  }
+  cat(sprintf("Top2vec model with %s topics", x$k), sep = "\n")
+  cat(sprintf("Topic distribution: %s", paste(round(prop.table(x$size), digits = 2), collapse = " ")), sep = "\n")
+  #m <- max(nchar(unlist(mapply(seq_along(x$topwords), x$topwords, FUN = function(i, data) sprintf("Topic %s: %s", i-1, paste(head(data$term, top_n[1]), collapse = " "))))))
+  for(i in seq_len(x$k)){
+    words <- head(x$topwords[[i]]$term, top_n[1])
+    docs  <- head(x$topdocs[[i]]$text, top_n[2])
+    
+    txt <- sprintf("Topic %s: %s", i-1, paste(words, collapse = " "))
+    cat(paste(rep("-", nchar(txt)), collapse = ""), sep = "\n")
+    cat(txt, sep = "\n")
+    cat(paste(rep("-", nchar(txt)), collapse = ""), sep = "\n")
+    cat(paste(sprintf("  (%s) %s", seq_along(docs), docs), collapse = "\n"), sep = "\n")
+  }
 }
